adff3b76481b128190a5a4627da9d0e0c8d85406
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2021-03-19 10:12:35 +0100

    added header component

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+      <h1 @click="$router.push('/contacts')">{{ title }}</h1>
+      <nav id="nav">
+        <button mode="round gray"><router-link to="/contacts/create">Add Contact</router-link></button>
+      </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: ['title'],
+})
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  margin: 2rem auto;
+  max-width: 40rem;
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 3px solid #8c8c8c;
+}
+
+header h1 {
+  color: #dcd9d9;
+  margin: 0;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+
+header h1:hover {
+  color: white;
+}
+a {
+  text-decoration: none;
+}
+
+a:active, a:visited {
+  color: black;
+}
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,15 +5,13 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    redirect: '/contacts'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/contacts',
+    name: 'Contacts',
+    component: () => import('../views/contacts/Contacts.vue')
   }
 ]
 
